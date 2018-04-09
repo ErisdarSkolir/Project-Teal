0fba8e4bb56548807c4081e638075b3d151b788e
DONMOYERLR17 <DONMOYERLR17@5CG731154D.GCC.edu>
2018-04-09 19:46:01 -0400

    Added entity class

--- src/gcc/edu/keen/Keen.java
+++ src/gcc/edu/keen/Keen.java
@@ -3,18 +3,18 @@ package gcc.edu.keen;
 public class Keen
 {
 	private boolean running = true;
-
+	
 	public static void main(String[] args)
 	{
 		Keen keen = new Keen();
 		keen.main();
 	}
-
+	
 	public void main()
 	{
-		while (running)
+		while(running)
 		{
-
+			
 		}
 	}
 }
---
+++ src/gcc/edu/keen/entities/Entity.java
@@ -0,0 +1,19 @@
+package gcc.edu.keen.entities;
+
+import org.joml.Vector2f;
+
+public class Entity
+{
+	private Vector2f position = new Vector2f();
+	private int texture;
+
+	public Entity()
+	{
+
+	}
+
+	public void tick()
+	{
+
+	}
+}
