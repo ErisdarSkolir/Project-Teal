74c77fcfc4a04fb4a837de28c890fdadcbe14433
DONMOYERLR17 <DONMOYERLR17@5CG731154D.GCC.edu>
2018-04-09 21:02:59 -0400

    Added a few more base classes.

--- src/gcc/edu/keen/Keen.java
+++ src/gcc/edu/keen/Keen.java
@@ -1,20 +1,34 @@
 package gcc.edu.keen;
 
+/**
+ * The main game object. Includes the main game loop and the current game state.
+ * 
+ * @author DONMOYERLR17
+ *
+ */
 public class Keen
 {
 	private boolean running = true;
-	
+
+	/**
+	 * Entrypoint.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		Keen keen = new Keen();
 		keen.main();
 	}
-	
+
+	/**
+	 * Main game loop. Calls the tick and render methods for the current game state
+	 */
 	public void main()
 	{
-		while(running)
+		while (running)
 		{
-			
+
 		}
 	}
 }
--- src/gcc/edu/keen/tiles/Tile.java
+++ src/gcc/edu/keen/tiles/Tile.java
@@ -1,10 +1,23 @@
 package gcc.edu.keen.tiles;
 
+import org.joml.Vector2f;
+import org.lwjgl.openvr.Texture;
+
+/**
+ * The tile class. A tile is a non-moving collisionable or non-collisionable
+ * game object.
+ * 
+ * @author DONMOYERLR17
+ *
+ */
 public class Tile
 {
+	private Vector2f position = new Vector2f();
 
-	public Tile()
+	private Texture texture;
+
+	public Tile(Texture texture)
 	{
-
+		this.texture = texture;
 	}
 }
