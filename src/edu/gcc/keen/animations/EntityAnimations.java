d3f01fe1ef2a318d42740542936bd01cc690ae8c
DONMOYERLR17 <DONMOYERLR17@5CG731154D.GCC.edu>
2018-04-29 16:05:43 -0400

    Made entity animations not conflict

--- src/edu/gcc/keen/animations/EntityAnimations.java
+++ src/edu/gcc/keen/animations/EntityAnimations.java
@@ -1,6 +1,5 @@
 package edu.gcc.keen.animations;
 
-
 public enum EntityAnimations implements Animation
 {
 	BULLET(0, 1, 2, 3, 4),
@@ -8,12 +7,9 @@ public enum EntityAnimations implements Animation
 	AMPTON_WALK_LEFT(18, 19, 20, 21),
 	AMPTON_WALK_RIGHT(25, 24, 23, 22),
 	AMPTON_ON_POLE(15),
-<<<<<<< HEAD
-	AMPTON_STUNNED(17);
-=======
+	AMPTON_STUNNED(17),
 	SPARKY_WALK_LEFT(0, 1, 2, 3),
-	SPARKY_WALK_RIGHT(10, 9 ,8 ,7);
->>>>>>> refs/heads/Keen_Animations
+	SPARKY_WALK_RIGHT(10, 9, 8, 7);
 
 	private int[] animation;
 
